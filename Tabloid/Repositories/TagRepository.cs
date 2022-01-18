35fd8525de7c624dc5abf2135ec2b80b302621be
Emily <emily.reed0913@gmail.com>
2022-01-18 14:55:53 -0600

    All tags are now visible to the user

---
+++ Tabloid/Repositories/TagRepository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tabloid.Models;
+
+namespace Tabloid.Repositories
+{
+    public class TagRepository : BaseRepository, ITagRepository
+    {
+        public TagRepository(IConfiguration config) : base(config) { }
+        public List<Tag> GetAllTags()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                      Select * from Tag
+                        ORDER BY name ASC";
+
+                    var tags = new List<Tag>();
+
+                    var reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        tags.Add(new Tag()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("name")),
+                        });
+                    }
+                    reader.Close();
+                    return tags;
+                }
+            }
+
+        }
+    }
+}
